2e1fc07e244b48803b75829679adbd4003304ad8
Hemi <zolotarenko.yevheniy1998@gmail.com>
2019-07-02 21:29:30 +0200

    request: { query: CURRENT_USER_QUERY},

---
+++ frontend/__tests__/PleaseSignIn.test.js
@@ -0,0 +1,32 @@
+import { mount } from "enzyme";
+import toJSON from "enzyme-to-json";
+import wait from "waait";
+import PleaseSignIn from "../components/PleaseSignIn";
+import { CURRENT_USER_QUERY } from "../components/User";
+import { MockedProvider } from "react-apollo/test-utils";
+import { fakeUser } from "../lib/testUtils";
+
+const notSignedInMocks = [
+  {
+    request: { query: CURRENT_USER_QUERY },
+    result: { data: { me: null } }
+  }
+];
+
+const signedInMocks = [
+  {
+    request: { query: CURRENT_USER_QUERY },
+    result: { data: { me: fakeUser() } }
+  }
+];
+
+describe("<PleaseSignIn />", () => {
+  it("renders the sign in dialog to logged out users", async () => {
+    const wrapper = mount(
+      <MockedProvider mocks={notSignedInMocks}>
+        <PleaseSignIn />
+      </MockedProvider>
+    );
+    console.log(wrapper.debug());
+  });
+});
