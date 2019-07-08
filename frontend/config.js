a0c3b4fffe639f3c2f248d03a91edd94f606b087
Hemi <zolotarenko.yevheniy1998@gmail.com>
2019-07-08 21:26:29 +0200

    Init

--- frontend/config.js
+++ frontend/config.js
@@ -1,4 +1,5 @@
 // This is client side config only - don't put anything in here that shouldn't be public!
 export const endpoint = `http://localhost:4444`;
+export const prodEndpoint = `https://store-wheels.herokuapp.com/`;
 export const perPage = 6;
 export const mainPageItems = 3;
